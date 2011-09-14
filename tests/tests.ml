ee71244ab4a50249641170046b4658710894e862
Sergei Lebedev <superbobry@gmail.com>
2011-09-14 13:52:43 +0400

    Added initial parseunparse implementation with kaputt

--- tests/tests.ml
+++ tests/tests.ml
@@ -2,10 +2,36 @@ open Kaputt.Abbreviations
 open Bson
 
 
+let show = function
+  | Double d -> Printf.sprintf "Double %f" d
+  | Int64 _ | Timestamp _ | Int32 _ | JSCodeWithScope _
+  | Symbol _ | JSCode _| Regex _ | Boolean _ | Datetime _
+  | ObjectId _ | BinaryData _ | Array _ | Document _ | String _
+  | Maxkey | Minkey | Null -> "<unknown>"
+
+
+module Gen = struct
+  include Gen
+
+  (* a fairly small size constraint for our "random" objects. *)
+  let size = make_int 1 512
+
+  let objectid = string (lift 12 "12") char
+  let cstring = string size char
+  let double = map1 (fun f -> Double f) show float
+
+  let element = choose_list [
+    double;
+  ]
+
+  let document = list size (zip2 cstring element)
+end
+
+
 let test_objectid = Test.make_random_test
   ~title:"ObjectId"
-  (Gen.string (Gen.lift 12 "12") Gen.char)
-  (fun s ->  ObjectId (to_objectid s))
+  Gen.objectid
+  (fun s -> ObjectId (to_objectid s))
   [Spec.always ==> (function
     | ObjectId s -> String.length (from_objectid s) = 12
     | Double _ | String _ | Document _ | Array _ | BinaryData _
@@ -15,4 +41,18 @@ let test_objectid = Test.make_random_test
   ]
 
 
-let () = Test.run_tests [test_objectid]
+let test_parseunparse = Test.make_random_test
+  ~title:"parse<->unparse"
+  Gen.document
+  (fun d -> d)
+  [Spec.always ==> (fun d ->
+    try
+      decode (encode d) = d
+    with
+      | MalformedBSON s -> print_endline s; false
+      | Stream.Failure -> print_endline "Stream.Failure"; false
+      | Stream.Error s -> Printf.printf "Stream.Error %s\n" s; false
+   )
+  ]
+
+let () = Test.run_tests [test_objectid; test_parseunparse]
