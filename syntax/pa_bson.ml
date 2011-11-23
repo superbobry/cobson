b0b5b0a7edcd87e4166e051db8cbc042182e4152
Sergei Lebedev <superbobry@gmail.com>
2011-11-23 19:50:40 +0400

    Okay, one more tech. commit -- at least it still works :)

--- syntax/pa_bson.ml
+++ syntax/pa_bson.ml
@@ -1,41 +1,55 @@
 open Camlp4.PreCast
-open Pa_type_conv
+
+module Gen = Pa_type_conv.Gen
 
 let raise_unsupported () =
   failwith "Unsupported use of bson (you can only use it on records)."
 
 
-module Gen_struct = struct
-  let record ~record_name _loc ty =
-    <:str_item<
-      open Bson
-    >>
+module Gen_bson_of = struct
+  let bson_of_record ~type_name _loc ty =
+    let fields = Ast.list_of_ctyp ty [] in
+    let aux = function
+      | <:ctyp@loc< $lid:name$ : string >> ->
+        <:expr@loc< ($str:name$, Bson.String $lid:name$) >>
+      | _ -> assert false
+    in
+
+    let loc = Ast.loc_of_ctyp ty in
+    let foo = List.map aux fields in
+      <:expr@loc< $foo$ >>, loc
   ;;
 
-  let mani ~record_name ty =
-    match ty with
-    | <:ctyp@loc< { $x$ } >> ->
-      record ~record_name loc x
-    | _ -> failwith "the right hand side of the manifest must be a record"
-
-  let bson_of_ty _loc ~record_name ~tps:_ ~rhs =
+  (* Generate code from type definition. *)
+  let bson_of_td loc ~type_name ~tps ~rhs =
     let unsupported = (fun _ _ -> raise_unsupported ()) in
     Gen.switch_tp_def
       ~alias:    unsupported
       ~sum:      unsupported
       ~variants: unsupported
-      ~mani:     (fun (_:Loc.t) _tp1 tp2 -> mani ~record_name tp2)
+      ~mani:     (fun _ _ _ -> raise_unsupported ())
       ~nil:      (fun _ -> raise_unsupported ())
-      ~record:   (record ~record_name)
+      ~record:   (bson_of_record ~type_name)
       rhs
 
-  let generate = function
-    | Ast.TyDcl (_loc, name, tps, rhs, _) -> bson_of_ty _loc ~record_name:name ~tps ~rhs
-    | Ast.TyAnd (_loc, _, _) as tds ->
-        ignore (_loc, tds);
-        failwith "Not supported"
-    | _ -> assert false
+  let generate tds =
+    let (binding, _loc) = match tds with
+      | Ast.TyDcl (loc, name, tps, rhs, _) -> bson_of_td loc ~type_name:name ~tps ~rhs
+      | Ast.TyAnd (_loc, _, _) as tds ->
+          ignore (_loc, tds);
+          failwith "Not supported"
+      | _ -> assert false
+    in
+
+      List.fold_right (fun x l -> <:expr< [$x$ :: $l$] >>) binding <:expr< [] >>
 end
 
 
-let () = add_generator "bson" Gen_struct.generate
+let () =
+  Pa_type_conv.add_generator
+    "bson"
+    (fun tds ->
+       let loc = Ast.loc_of_ctyp tds in
+       let foo = Gen_bson_of.generate tds in
+         <:str_item@loc< value x = $foo$ >>
+    )
